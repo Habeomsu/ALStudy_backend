5cb99224497b2a28ab75d0e91373707c7d0f50c2
Habeomsu <hbs5186@naver.com>
2024-12-30 10:48:09 +0900

    프로젝트 생성

---
+++ Als/src/main/java/main/als/testController.java
@@ -0,0 +1,17 @@
+package main.als;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class testController {
+
+    @GetMapping()
+    public String test() {
+        return "test is running";
+    }
+}

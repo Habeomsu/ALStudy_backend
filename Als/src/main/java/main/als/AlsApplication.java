eabf6ab03bf79974170c46ffa6d3c2614425c3fd
Habeomsu <hbs5186@naver.com>
2025-01-27 14:06:06 +0900

    스터디기간지난 그룹 삭제

--- Als/src/main/java/main/als/AlsApplication.java
+++ Als/src/main/java/main/als/AlsApplication.java
@@ -2,8 +2,10 @@ package main.als;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AlsApplication {
 
     public static void main(String[] args) {

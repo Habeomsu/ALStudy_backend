898332217721867aa24ccea9f2cd0a6d4e40c2ce
Habeomsu <hbs5186@naver.com>
2025-03-31 21:36:40 +0900

    그룹 리포지토리 테스트 구현

--- Als/src/test/java/main/als/usertest/UserRepositoryTest.java
+++ Als/src/test/java/main/als/usertest/UserRepositoryTest.java
@@ -10,11 +10,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 public class UserRepositoryTest {
 
     @Autowired

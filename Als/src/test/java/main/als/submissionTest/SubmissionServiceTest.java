a487bed5e0c8224e49dcfccb297a727f525168f0
Habeomsu <hbs5186@naver.com>
2025-05-09 15:56:37 +0900

    #51 Submission getAll 서비스 테스트

--- Als/src/test/java/main/als/submissionTest/SubmissionServiceTest.java
+++ Als/src/test/java/main/als/submissionTest/SubmissionServiceTest.java
@@ -5,6 +5,8 @@ import main.als.apiPayload.exception.GeneralException;
 import main.als.aws.s3.AmazonS3Manager;
 import main.als.group.entity.Group;
 import main.als.group.repository.UserGroupRepository;
+import main.als.page.PostPagingDto;
+import main.als.problem.dto.SubmissionResponseDto;
 import main.als.problem.entity.*;
 import main.als.problem.repository.GroupProblemRepository;
 import main.als.problem.repository.SubmissionRepository;
@@ -21,6 +23,9 @@ import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
@@ -444,6 +449,253 @@ public class SubmissionServiceTest {
             assertEquals(ErrorStatus._FLASK_SERVER_ERROR.getHttpStatus(), exception.getErrorReasonHttpStatus().getHttpStatus());
 
         }
+    }
+
+    @Test
+    @DisplayName("getAll 테스트 - (성공)")
+    public void getAllSuccessTest(){
+
+        String username = "test";
+        Long groupProblemId = 1L;
+
+        Group group = Group.builder()
+                .id(1L)
+                .build();
+
+        User user = User.builder()
+                .id(1L)
+                .username(username)
+                .build();
+
+        TestCase testCase1 = TestCase.builder()
+                .id(1L)
+                .build();
+
+        TestCase testCase2 = TestCase.builder()
+                .id(2L)
+                .build();
+
+        Problem problem = Problem.builder()
+                .id(1L)
+                .testCases(List.of(testCase1, testCase2))
+                .build();
+
+        GroupProblem groupProblem = GroupProblem.builder()
+                .id(groupProblemId)
+                .group(group)
+                .deadline(now.plusDays(1))
+                .problem(problem)
+                .build();
+
+        Submission submission1 = Submission.builder()
+                .id(1L)
+                .user(user)
+                .groupProblem(groupProblem)
+                .language("python")
+                .status(SubmissionStatus.FAILED)
+                .submissionTime(LocalDateTime.now())
+                .build();
+
+        Submission submission2 = Submission.builder()
+                .id(2L)
+                .user(user)
+                .groupProblem(groupProblem)
+                .language("python")
+                .status(SubmissionStatus.SUCCEEDED)
+                .submissionTime(LocalDateTime.now())
+                .build();
+
+        PostPagingDto.PagingDto pagingDto = PostPagingDto.PagingDto.builder()
+                .page(0)
+                .size(10)
+                .sort("DESC")
+                .build();
+
+        Page<Submission> page = new PageImpl<>(List.of(submission1, submission2));
+
+        when(userRepository.findByUsername(username)).thenReturn(user);
+        when(groupProblemRepository.findById(groupProblemId)).thenReturn(Optional.ofNullable(groupProblem));
+        when(userGroupRepository.existsByGroupIdAndUserUsername(group.getId(), username)).thenReturn(true);
+        when(submissionRepository.findByUserAndGroupProblem(eq(user),eq(groupProblem),any(Pageable.class))).thenReturn(page);
+
+        SubmissionResponseDto.SearchSubmissionDto result =
+                submissionService.getAll(groupProblemId, username, pagingDto);
+
+        assertNotNull(result);
+        assertEquals(2, result.getTotalElements());
+        assertEquals("python", result.getSubmissionResDtos().get(0).getLanguage());
+
+        verify(submissionRepository).findByUserAndGroupProblem(eq(user), eq(groupProblem), any(Pageable.class));
+
+    }
+
+    @Test
+    @DisplayName("getAll 테스트 - (실패 - 사용자 없음)")
+    public void getAllFailTest1(){
+
+        String username = "test";
+        Long groupProblemId = 1L;
+
+        PostPagingDto.PagingDto pagingDto = PostPagingDto.PagingDto.builder()
+                .page(0)
+                .size(10)
+                .sort("DESC")
+                .build();
+
+        when(userRepository.findByUsername(username)).thenReturn(null);
+
+        GeneralException exception = assertThrows(GeneralException.class, () -> submissionService.getAll(groupProblemId,username, pagingDto));
+
+        assertFalse(exception.getErrorReasonHttpStatus().getIsSuccess());
+        assertEquals(ErrorStatus._USERNAME_NOT_FOUND.getCode(), exception.getErrorReasonHttpStatus().getCode());
+        assertEquals(ErrorStatus._USERNAME_NOT_FOUND.getMessage(), exception.getErrorReasonHttpStatus().getMessage());
+        assertEquals(ErrorStatus._USERNAME_NOT_FOUND.getHttpStatus(), exception.getErrorReasonHttpStatus().getHttpStatus());
+
+    }
+
+    @Test
+    @DisplayName("getAll 테스트 - (실패 - 그룹문제 없음)")
+    public void getAllFailTest2(){
+
+        String username = "test";
+        Long groupProblemId = 1L;
+
+        Group group = Group.builder()
+                .id(1L)
+                .build();
+
+        User user = User.builder()
+                .id(1L)
+                .username(username)
+                .build();
+
+        TestCase testCase1 = TestCase.builder()
+                .id(1L)
+                .build();
+
+        TestCase testCase2 = TestCase.builder()
+                .id(2L)
+                .build();
+
+        Problem problem = Problem.builder()
+                .id(1L)
+                .testCases(List.of(testCase1, testCase2))
+                .build();
+
+        GroupProblem groupProblem = GroupProblem.builder()
+                .id(groupProblemId)
+                .group(group)
+                .deadline(now.plusDays(1))
+                .problem(problem)
+                .build();
+
+        Submission submission1 = Submission.builder()
+                .id(1L)
+                .user(user)
+                .groupProblem(groupProblem)
+                .language("python")
+                .status(SubmissionStatus.FAILED)
+                .submissionTime(LocalDateTime.now())
+                .build();
+
+        Submission submission2 = Submission.builder()
+                .id(2L)
+                .user(user)
+                .groupProblem(groupProblem)
+                .language("python")
+                .status(SubmissionStatus.SUCCEEDED)
+                .submissionTime(LocalDateTime.now())
+                .build();
+
+        PostPagingDto.PagingDto pagingDto = PostPagingDto.PagingDto.builder()
+                .page(0)
+                .size(10)
+                .sort("DESC")
+                .build();
+
+        when(userRepository.findByUsername(username)).thenReturn(user);
+        when(groupProblemRepository.findById(groupProblemId)).thenReturn(Optional.empty());
+
+        GeneralException exception = assertThrows(GeneralException.class, () -> submissionService.getAll(groupProblemId,username, pagingDto));
+
+        assertFalse(exception.getErrorReasonHttpStatus().getIsSuccess());
+        assertEquals(ErrorStatus._NOT_FOUND_GROUPPROBLEM.getCode(), exception.getErrorReasonHttpStatus().getCode());
+        assertEquals(ErrorStatus._NOT_FOUND_GROUPPROBLEM.getMessage(), exception.getErrorReasonHttpStatus().getMessage());
+        assertEquals(ErrorStatus._NOT_FOUND_GROUPPROBLEM.getHttpStatus(), exception.getErrorReasonHttpStatus().getHttpStatus());
+
+    }
+
+    @Test
+    @DisplayName("getAll 테스트 - (실패 - 유저그룹이 아님)")
+    public void getAllFailTest3(){
+
+        String username = "test";
+        Long groupProblemId = 1L;
+
+        Group group = Group.builder()
+                .id(1L)
+                .build();
+
+        User user = User.builder()
+                .id(1L)
+                .username(username)
+                .build();
+
+        TestCase testCase1 = TestCase.builder()
+                .id(1L)
+                .build();
+
+        TestCase testCase2 = TestCase.builder()
+                .id(2L)
+                .build();
+
+        Problem problem = Problem.builder()
+                .id(1L)
+                .testCases(List.of(testCase1, testCase2))
+                .build();
+
+        GroupProblem groupProblem = GroupProblem.builder()
+                .id(groupProblemId)
+                .group(group)
+                .deadline(now.plusDays(1))
+                .problem(problem)
+                .build();
+
+        Submission submission1 = Submission.builder()
+                .id(1L)
+                .user(user)
+                .groupProblem(groupProblem)
+                .language("python")
+                .status(SubmissionStatus.FAILED)
+                .submissionTime(LocalDateTime.now())
+                .build();
+
+        Submission submission2 = Submission.builder()
+                .id(2L)
+                .user(user)
+                .groupProblem(groupProblem)
+                .language("python")
+                .status(SubmissionStatus.SUCCEEDED)
+                .submissionTime(LocalDateTime.now())
+                .build();
+
+        PostPagingDto.PagingDto pagingDto = PostPagingDto.PagingDto.builder()
+                .page(0)
+                .size(10)
+                .sort("DESC")
+                .build();
+
+        when(userRepository.findByUsername(username)).thenReturn(user);
+        when(groupProblemRepository.findById(groupProblemId)).thenReturn(Optional.of(groupProblem));
+        when(userGroupRepository.existsByGroupIdAndUserUsername(group.getId(), username)).thenReturn(false);
+
+        GeneralException exception = assertThrows(GeneralException.class, () -> submissionService.getAll(groupProblemId,username, pagingDto));
+
+        assertFalse(exception.getErrorReasonHttpStatus().getIsSuccess());
+        assertEquals(ErrorStatus._NOT_IN_USERGROUP.getCode(), exception.getErrorReasonHttpStatus().getCode());
+        assertEquals(ErrorStatus._NOT_IN_USERGROUP.getMessage(), exception.getErrorReasonHttpStatus().getMessage());
+        assertEquals(ErrorStatus._NOT_IN_USERGROUP.getHttpStatus(), exception.getErrorReasonHttpStatus().getHttpStatus());
+
     }
 
 }
